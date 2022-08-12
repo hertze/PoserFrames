b5d565b6c854a98947b9ddc1e5f3bc0b5a41f016
Joakim Hertze <joakim@hertze.com>
2022-08-12 15:29:48 +0200

    Updates.

--- ExtractPath.jsx
+++ ExtractPath.jsx
@@ -13,7 +13,10 @@ var pathObj = app.activeDocument.pathItems.getByName('Frame');
 
 for (var s = 0; s < pathObj.subPathItems.length; s++) { // Loopa genom alla SubPathItems
     
+    alert(pathObj.subPathItems[s].typename)
+    
     for (var i = 0; i < pathObj.subPathItems[s].pathPoints.length; i++) { // Loopa genom alla pathPoints i SubPathitem
+        
         
         var thisPathPoint = pathObj.subPathItems[s].pathPoints[i].kind + " " + pathObj.subPathItems[s].pathPoints[i].anchor +  " " + pathObj.subPathItems[s].pathPoints[i].leftDirection + " " + pathObj.subPathItems[s].pathPoints[i].rightDirection + " " + pathObj.subPathItems[s].pathPoints[i].typename;
         
