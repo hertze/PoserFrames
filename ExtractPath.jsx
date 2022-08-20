01802da02b8a293be786cd1f0ac5f57f0648a570
Joakim Hertze <joakim@hertze.com>
2022-08-20 12:25:51 +0200

    Update ExtractPath.jsx

--- ExtractPath.jsx
+++ ExtractPath.jsx
@@ -14,9 +14,9 @@ ExportFile.open("a", "TEXT");
 // Get paths in document
 var pathObj = app.activeDocument.pathItems.getByName('Frame');
 
-for (var s = 0; s < pathObj.subPathItems.length; s++) { // Loopa genom alla SubPathItems
+for (var s = 0; s < pathObj.subPathItems.length; s++) { // Loop through all SubPathItems
     
-    for (var i = 0; i < pathObj.subPathItems[s].pathPoints.length; i++) { // Loopa genom alla pathPoints i SubPathitem
+    for (var i = 0; i < pathObj.subPathItems[s].pathPoints.length; i++) { // Loop through all pathPoints in SubPathitem
         
         
         var thisPathPoint = pathObj.subPathItems[s].pathPoints[i].kind + " " + pathObj.subPathItems[s].pathPoints[i].anchor +  " " + pathObj.subPathItems[s].pathPoints[i].leftDirection + " " + pathObj.subPathItems[s].pathPoints[i].rightDirection + " " + pathObj.subPathItems[s].pathPoints[i].typename;
