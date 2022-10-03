a4c9536bcf6c8e04cc70cd5ae1e7b8e6b23a825e
Joakim Hertze <joakim@hertze.com>
2022-10-03 07:14:55 +0200

    Update PoserFrames.jsx

--- PoserFrames.jsx
+++ PoserFrames.jsx
@@ -289,10 +289,10 @@ function moveNeg(thisFormat) {
 		if (ratio > 1) {
 			// Portrait
 			var movement_horisontal = generateRandomInteger(5, 10) * 0.1 * doc_width * 0.015 * -1;
-			var movement_vertical = generateRandomInteger(1, 10) * 0.1 * doc_height * 0.005;
+			var movement_vertical = generateRandomInteger(1, 10) * 0.1 * doc_height * 0.001;
 		} else {
 			// Landscape
-			var movement_horisontal = generateRandomInteger(1, 10) * 0.1 * doc_width * 0.005 * -1;
+			var movement_horisontal = generateRandomInteger(1, 10) * 0.1 * doc_width * 0.001 * -1;
 			var movement_vertical = generateRandomInteger(5, 10) * 0.1 * doc_height * 0.015 * 1;
 		}
 	} else {
