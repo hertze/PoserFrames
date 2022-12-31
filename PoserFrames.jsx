f196ece891d24c933fa40e8ef37ac03ad4234751
Joakim Hertze <joakim@hertze.com>
2022-12-31 13:52:05 +0100

    Fixed bug with random rotation.

--- PoserFrames.jsx
+++ PoserFrames.jsx
@@ -502,12 +502,12 @@ function createPath(thisFrame, pathName) {
 
 function decideRotation(pathKind) {
 	
-	var randRotation = generateRandomInteger(1, 10) / 10 * 0.2 - 0.1; // How much random rotation to add, between -0.1 and 0.1 deg.
+	var randRotation = generateRandomInteger(1, 11) / 10 * 0.2 - 0.1; // How much random rotation to add, between -0.1 and 0.1 deg.
 	if (app.activeDocument.height > app.activeDocument.width ) {
 		// Portrait
 		if (pathKind == "frame") {
 			// Only frame should have random rotation or 180 flip.
-			if (generateRandomInteger(1, 3) == 3 && thisFormat != "45") {
+			if (generateRandomInteger(1, 4) == 3 && thisFormat != "45") {
 				app.activeDocument.selection.rotateBoundary(270 + randRotation, AnchorPosition.MIDDLECENTER);
 			} else {
 				app.activeDocument.selection.rotateBoundary(90 + randRotation, AnchorPosition.MIDDLECENTER);
