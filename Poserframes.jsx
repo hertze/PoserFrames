80a52ecc224ccf97b4b4e8e43c4c5169995c653d
Joakim Hertze <joakim@hertze.com>
2024-03-21 17:00:14 +0100

    Update Poserframes.jsx

--- Poserframes.jsx
+++ Poserframes.jsx
@@ -25,7 +25,7 @@ var halation = true;
 
 var artifacts = true;
 
-var mask_variant_35mm = 5;
+var mask_variant_35mm = 1;
 var mask_variant_645 = 1;
 var mask_variant_67 = 1;
 var mask_variant_45 = 1;
