1e1942758987aaf7d6e76cc30f17bf843035112b
Joakim Hertze <joakim@hertze.com>
2024-08-23 19:42:19 +0200

    Update Poserframes.jsx

--- Poserframes.jsx
+++ Poserframes.jsx
@@ -1192,7 +1192,7 @@ function renderFilmBurn() {
 		} else {
 			myColor_filmburn_red.hsb.hue = generateRandomInteger(0, 10);
 			myColor_filmburn_red.hsb.saturation = generateRandomInteger(70, 80, "middle");
-			myColor_filmburn_red.hsb.brightness = generateRandomInteger(70, 80, "lower");
+			myColor_filmburn_red.hsb.brightness = generateRandomInteger(40, 60, "lower");
 
 			myColor_filmburn_orange.hsb.hue = 45 - myColor_filmburn_red.hsb.hue;
 			myColor_filmburn_orange.hsb.saturation = generateRandomInteger(2, 80, "lower");
